d536b4d451b88dc8d11c7d250eac55a2fb5a3537
Maximilian Hils <git@maximilianhils.com>
2014-10-04 12:10:16 +0200

    ws base

--- mitm.py
+++ mitm.py
@@ -1,5 +1,50 @@
+import json
+import threading
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+
+clients = []
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(request):
+        request.render("index.html")
+
+
+class WebSocketChatHandler(tornado.websocket.WebSocketHandler):
+    def open(self, *args):
+        print("open", "WebSocketChatHandler")
+        clients.append(self)
+
+    @classmethod
+    def broadcast(cls, type, data):
+        message = json.dumps({
+            "type": type,
+            "data": data
+        })
+        for client in clients:
+            client.write_message(message)
+
+    def on_close(self):
+        clients.remove(self)
+
+
+app = None
+app_thread = None
+ws = None
+
 def start(ctx, argv):
-    print "foo"
+    global app, app_thread
+    app = tornado.web.Application([(r'/events', WebSocketChatHandler), (r'/', IndexHandler)])
+    app.listen(8085)
+
+    app_thread = threading.Thread(target=tornado.ioloop.IOLoop.instance().start)
+    app_thread.start()
+    print "started"
+
 
 def response(ctx, flow):
+    WebSocketChatHandler.broadcast("flow", flow.get_state(short=True))
     print "Received flow: %s" % flow
