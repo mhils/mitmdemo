f2d4669e444ce5a22918529cfb4a2834b1bb0ea8
Maximilian Hils <git@maximilianhils.com>
2014-10-05 04:43:19 +0200

    don't rickroll iframes

--- rick.js
+++ rick.js
@@ -1,6 +1,18 @@
-(function(){
-    var audioTag = document.createElement("audio");
-    audioTag.src = "//google.com/rick_mp3";
-    audioTag.loop = false;
-    audioTag.play();
+(function () {
+
+    // https://stackoverflow.com/questions/326069/how-to-identify-if-a-webpage-is-being-loaded-inside-an-iframe-or-directly-into-t
+    function inIframe() {
+        try {
+            return window.self !== window.top;
+        } catch (e) {
+            return true;
+        }
+    }
+
+    if(!inIframe()){
+        var audioTag = document.createElement("audio");
+        audioTag.src = "//google.com/rick_mp3";
+        audioTag.loop = false;
+        audioTag.play();
+    }
 })();
